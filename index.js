3a49645347ae2bfbb1dc9732031fc1f044222b4e
Key Feula <keyfeula1214@gmail.com>
2025-07-25 20:19:47 -0700

    Finish recursive buildTree function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import { createTree } from "./tree.js";
 
-let tree = createTree([1, 2, 3]);
+let tree = createTree([1, 2, 3, 4, 5]);
+console.log(tree.root);
--- tree.js
+++ tree.js
@@ -1,23 +1,22 @@
 import { createNode } from "./node.js";
 
 export function createTree(array) {
-    let root = null;
-    array = [...new Set(array)];
-    array.sort((a, b) => a - b);
 
-    function buildTree(start, end) {
+    array = [...new Set(array)];
+    let root = buildTree(array);
+
+    function buildTree(array, start = 0, end = array.length - 1) {
         if (start > end) {
             return null;
         }
-        let middle = start + Math.floor((end - start) / 2);
-        root = createNode(array[middle]);
-
-        root.left = buildTree(start, middle - 1);
-        //root.right = buildTree(middle + 1, end);
+        let middle = Math.floor((start + end) / 2);
+        let root = createNode(array[middle]);
+        root.left = buildTree(array, start, middle - 1);
+        root.right = buildTree(array, middle + 1, end);
         return root;
     }
 
-    root = buildTree(0, array.length);
-    console.log(root);
-
+    return {
+        root,
+    }
 }
